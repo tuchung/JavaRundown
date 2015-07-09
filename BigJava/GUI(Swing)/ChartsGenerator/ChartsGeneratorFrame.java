d5d6e17e367ef766b56404f1b50e17e8e94d4137
Tu Chung <tuthienchung@gmail.com>
2015-07-09 18:11:20 -0400

    Created ChartsGenerator application

---
+++ BigJava/GUI(Swing)/ChartsGenerator/ChartsGeneratorFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+
+public class ChartsGeneratorFrame extends JFrame {
+	private JLabel Image;
+	public ChartsGeneratorFrame() {
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
