52d92d032f48c14db3070258a34b3d60a0d67fce
Tu Chung <tuthienchung@gmail.com>
2015-07-09 00:10:47 -0400

    Create new Calculator Application

---
+++ BigJava/GUI(Swing)/src/CalculatorFrameView.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+
+public class CalculatorFrameView {
+
+	public static void main(String[] args) {
+		CalculatorFrame Calculator = new CalculatorFrame();
+		Calculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Calculator.setVisible(true);
+	}
+
+}
